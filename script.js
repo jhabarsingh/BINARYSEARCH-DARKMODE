aea4339346dca0c2bc3d0aa0866ba30bdb423d64
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-07-20 12:34:32 +0530

    Added Recursive Dark Mode Script

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-document.querySelector("#root").style.backgroundColor = "#aaa";
-document.querySelector("#root").style.backgroundColor = "#000";
+function allDescendants(node) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+        var child = node.childNodes[i];
+        allDescendants(child);
+        if(child.style) {
+            child.style.backgroundColor = "#2B2B2B";
+            
+            if(child.className != "view-lines")
+                 child.style.color = "white";
+        }
+    }
+}
+  
+var children = document.querySelector("body");
+
+
+setInterval(() => {
+    allDescendants(children);
+}, 10)
